f0c595f0c9bd526764d9068b3b80a58146aa29a8
NastyaQ <nastyaq92@gmail.com>
2021-04-09 11:34:31 +0300

    adaptive

--- js/script.js
+++ js/script.js
@@ -1,27 +1,65 @@
 svg4everybody();
 
 const swiper = new Swiper('.channel-slider', {
-  // Optional parameters
-  loop: true,
-  slidesPerView: 6,
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.slider-buttons1 .channel-button-next',
-    prevEl: '.slider-buttons1 .channel-button-prev',
-  },
+    // Optional parameters
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 20,
+    breakpoints: {
+        1900: {
+            slidesPerView: 6
+        },
+        1600: {
+            slidesPerView: 5
+        }, 
+        1100: {
+            slidesPerView: 4
+        },
+        992: {
+            slidesPerView: 3
+        },
+        760: {
+            slidesPerView: 2
+        },        
+    },
+    // Navigation arrows
+    navigation: {
+        nextEl: '.slider-buttons1 .channel-button-next',
+        prevEl: '.slider-buttons1 .channel-button-prev',
+    },
 
 });
 
 const recommended = new Swiper('.channel-slider2', {
-  // Optional parameters
-  loop: true,
-  slidesPerView: 3,
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.slider-buttons2 .channel-button-next',
-    prevEl: '.slider-buttons2 .channel-button-prev',
-  },
+    // Optional parameters
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 20,
+    breakpoints: {
+        1600: {
+            slidesPerView: 3
+        }, 
+        1100: {
+            slidesPerView: 2
+        },      
+    },
+    // Navigation arrows
+    navigation: {
+        nextEl: '.slider-buttons2 .channel-button-next',
+        prevEl: '.slider-buttons2 .channel-button-prev',
+    },
 
 });
+
+const searchBtn = document.querySelector('.mobile-search');
+const mobileSearch = document.querySelector('.input-group');
+searchBtn.addEventListener('click', () => {
+    mobileSearch.classList.toggle('is-open');
+})
+
+if(document.documentElement.scrollWidth <= 640){
+    console.log('asdasd');
+    console.log(swiper);
+    swiper.destroy();
+    recommended.destroy();
+}
